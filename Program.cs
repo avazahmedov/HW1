50bc520a0f59bd85dd7eb6c6f0488cc4242f4cc6
Avaz <qoraqosh1993@mail.ru>
2022-12-01 00:42:14 +0500

    1st done

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int Maximum = (number1);
+int Minimum = (number2);
+
+if (number2>number1)
+{
+    Maximum = Minimum;
+    Minimum = number1;
+    Console.WriteLine("Максимальное число " + Maximum);
+    Console.WriteLine("Минимальное число " + Minimum);
+}
+else
+{
+    Console.WriteLine("Максимальное число " + Maximum);
+    Console.WriteLine("Минимальное число " + Minimum); 
+}
