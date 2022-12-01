47ee637c5066fec1f0b96d413e19e6c37b7a872a
Avaz <qoraqosh1993@mail.ru>
2022-12-01 23:38:13 +0500

    2nd done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите первое число ");
+﻿/*
+Console.WriteLine("Введите первое число ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int number2 = Convert.ToInt32(Console.ReadLine());
@@ -18,3 +19,27 @@ else
     Console.WriteLine("Максимальное число " + Maximum);
     Console.WriteLine("Минимальное число " + Minimum); 
 }
+*/
+
+/*
+Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int Maximum = (number1);
+int Minimum = (number2);
+
+if (Minimum>Maximum)
+{
+   Maximum = Minimum;
+   Minimum = number1;
+}
+if (number3>Maximum)
+{
+    Maximum = number3;
+}
+    Console.WriteLine("Максимальное число " + Maximum);
+    */
