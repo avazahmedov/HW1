d492e0854930f6d240ccb82bd66f654748a6c5be
Avaz <qoraqosh1993@mail.ru>
2022-12-02 00:01:17 +0500

    4th done

--- Program.cs
+++ Program.cs
@@ -57,3 +57,20 @@ else
     Console.WriteLine(number + " это нечетное число ");
 }
 */
+
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+
+while (number<=N)
+{
+    if (number % 2 == 0)
+    {
+        Console.Write(number + ",");
+        number+=2;
+    }
+    else 
+    {
+        number++;
+    }
+}
